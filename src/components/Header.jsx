030696ce95c8a302a846f010611336b84e183bfb
saeidpourrajabi <pourrajabisaeid@gmail.com>
2024-05-19 19:58:46 +0330

    add header course status

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
-
+const coursestatus = ["All","Active","Uncoming","Completed"];
 
 function Header() {
   return (
-    <div><h1>My Courses (3)</h1></div>
+    <div>
+      <h1>My Courses (3)</h1>
+      <div className="course-status">
+        {
+          coursestatus.map((s)=>{
+            return <div key={s}>{s}</div>
+          })
+        }
+      </div>
+      </div>
   )
 }
 
